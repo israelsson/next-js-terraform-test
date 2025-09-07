87624e8d8806812328d7014f32a071dcd6aaf997
israelsson <anders.i84@gmail.com>
2025-09-07 10:55:10 +0200

    New setup for: refs/heads/patch-1757235276869

--- main.tf
+++ main.tf
@@ -21,3 +21,8 @@ resource "vercel_project_domain" "example" {
   project_id = vercel_project.example.id
   domain     = "myproject-domain.vercel.app"
 }
+
+resource "vercel_project_domain" "example" {
+  project_id = vercel_project.example.id
+  domain     = "myproject-domain.vercel.app"
+}
