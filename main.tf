62e45712e856a61e329b28498daed5aeefd6f99b
israelsson <anders.i84@gmail.com>
2025-09-07 16:23:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,28 +6,3 @@ terraform {
     }
   }
 }
-
-resource "vercel_project_domain" "example" {
-  project_id = vercel_project.example.id
-  domain     = "myproject-domain.vercel.app"
-}
-
-resource "vercel_project_domain" "example" {
-  project_id = vercel_project.example.id
-  domain     = "myproject-domain.vercel.app"
-}
-
-resource "vercel_project_domain" "example" {
-  project_id = vercel_project.example.id
-  domain     = "myproject-domain.vercel.app"
-}
-
-resource "vercel_project_domain" "example" {
-  project_id = vercel_project.example.id
-  domain     = "myproject-domain.vercel.app"
-}
-
-resource "vercel_project_domain" "example" {
-  project_id = vercel_project.example.id
-  domain     = "myproject-domain.vercel.app"
-}
